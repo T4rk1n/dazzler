cfffc4446139f0a42d9c4baab5ad6e890bdce717
t4rk <t4rk@outlook.com>
2019-07-06 13:37:21 -0400

    :construction: Add normalize.css to renderer requirements.

--- dazzler/_assets.py
+++ dazzler/_assets.py
@@ -7,6 +7,8 @@ root_path = get_package_path('dazzler')
 assets_path = os.path.abspath(os.path.join(root_path, '..', 'assets'))
 assets_dist_path = os.path.join(assets_path, 'dist')
 assets_dev_path = os.path.join(assets_path, 'dev')
+vendors_path = os.path.join(assets_path, 'vendors')
+
 
 with open(os.path.join(assets_dist_path, 'assets.json')) as _file:
     assets = json.load(_file)['chunks']
