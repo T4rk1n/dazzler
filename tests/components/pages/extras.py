37994de01f5352b30390e77b04e3ce1f9e5a1799
t4rk <t4rk@outlook.com>
2019-09-04 19:26:21 -0400

    :construction: Add extra component test page.

---
+++ tests/components/pages/extras.py
@@ -0,0 +1,35 @@
+"""
+Page extras of dazzler
+Created 2019-09-03
+
+Gallery of extra components.
+"""
+from dazzler.components import core, extra
+from dazzler.system import Page, Trigger, BindingContext
+
+page = Page(
+    __name__,
+    core.Container([
+        core.Button('Notify', identity='notify-btn'),
+        extra.Notice('Notice', identity='notice'),
+        extra.Spinner(identity='spin'),
+        core.Container([
+            core.Container('lorem', style={'padding': '2rem'})
+            for _ in range(20)
+        ]),
+        extra.Sticky(
+            extra.Drawer(core.Container('drawed'), side='bottom', identity='draw'),
+            identity='stick',
+            bottom='0',
+        ),
+    ])
+)
+
+
+@page.bind(Trigger('notify-btn', 'clicks'))
+async def on_notify(ctx: BindingContext):
+    await ctx.set_aspect(
+        'notice',
+        body=f'Clicked {ctx.trigger.value}',
+        displayed=True,
+    )
