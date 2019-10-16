0aa38fae6b6b84d408bb2a1596f49f81dd8b7842
t4rk <t4rk@outlook.com>
2019-10-15 20:45:46 -0400

    :construction: Add get_member.

--- dazzler/tools/_utils.py
+++ dazzler/tools/_utils.py
@@ -5,13 +5,36 @@ import importlib
 __all__ = [
     'get_package_path',
     'replace_all',
-    'format_tag'
+    'format_tag',
+    'get_member',
 ]
 
 
 def get_package_path(name):
     module = importlib.import_module(name)
     return os.path.dirname(module.__file__)
+
+
+def get_member(member_path: str):
+    """
+    Import a module and retrieve a member from a module dot notation path
+    separated by an ``:`` with the instance name.
+
+    (eg: ``package.module:instance``)
+
+    :param member_path: A string path with pattern
+        ``package.module:member``
+    :return:
+    """
+    split = member_path.split(':')
+    if len(split) != 2:
+        raise ImportError(
+            'Invalid instance path syntax '
+            '(eg: `package.module:instance`)'
+        )
+    module_name, instance_name = split
+    module = importlib.import_module(module_name)
+    return getattr(module, instance_name, None)
 
 
 def replace_all(template, **kwargs):
