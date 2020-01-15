549e6fc1278709065bf0beda52f599210b42f9f5
t4rk <t4rk@outlook.com>
2020-01-14 21:23:19 -0500

    :white_check_mark: Add assertions for regex states.

--- tests/apps/pages/regex_bindings.py
+++ tests/apps/pages/regex_bindings.py
@@ -5,7 +5,7 @@ Created 2020-01-12
 import re
 
 from dazzler.components import core
-from dazzler.system import Page, Trigger, BindingContext
+from dazzler.system import Page, Trigger, BindingContext, State
 
 page = Page(
     __name__,
@@ -14,13 +14,32 @@ page = Page(
         core.Button('btn2', identity='btn2'),
         core.Container(identity='output1'),
         core.Container(identity='output2'),
+        core.Input(identity='state1'),
+        core.Store(data='store', identity='state2'),
+        core.Container(identity='state-output')
     ])
 )
 
 
-@page.bind(Trigger(r'btn\d', 'clicks', regex=True))
+@page.bind(
+    Trigger(r'btn\d', 'clicks', regex=True),
+    State(r'state\d', '(value|data)', regex=True)
+)
 async def on_any_click(ctx: BindingContext):
     await ctx.set_aspect(
         re.compile(r'output\d'),
         children=f'clicked from button {ctx.trigger.identity}'
     )
+    output = []
+    for identity, aspects in ctx.states.items():
+
+        for aspect_name, aspect_value in aspects.items():
+            output.append(
+                core.Container(
+                    f'{aspect_name}@{identity}: {aspect_value}',
+                    identity=f'{aspect_name}-{identity}-output')
+            )
+
+    # FIXME Setting the array directly on children trigger
+    #  the same identity bug #53
+    await ctx.set_aspect('state-output', children=core.Container(output))
