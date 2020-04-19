8fb289aa6e0ebbba7e8e5ec5f796432f31ded049
t4rk <t4rk@outlook.com>
2020-04-19 13:23:35 -0400

    :white_check_mark: Add test_treeview.

--- tests/components/test_extra.py
+++ tests/components/test_extra.py
@@ -39,3 +39,44 @@ async def test_pager(start_page, browser):
             await asyncio.sleep(0.5)
         except TimeoutException:
             assert stop == len(pager.items)
+
+
+@pytest.mark.async_test
+async def test_treeview(start_page, browser):
+    from tests.components.pages.treeview import page
+
+    await start_page(page)
+
+    # Click on first nest
+    await browser.click('.level-0:nth-child(2)')
+    await browser.wait_for_text_to_equal('#output', 'nest')
+
+    # Expect sub items to show up
+    await browser.wait_for_text_to_equal('.level-1:nth-child(1)', 'nested1')
+
+    # Subnest
+    await browser.click('.level-1:nth-child(3)')
+    await browser.wait_for_text_to_equal('#output', 'nest.subnest')
+
+    await browser.click('.level-2:nth-child(1)')
+    await browser.wait_for_text_to_equal('#output', 'nest.subnest.sub1')
+
+    # Unselect
+    await browser.click('.level-2:nth-child(1)')
+    await browser.wait_for_text_to_equal('#output', 'nest.subnest')
+
+    # Click on level before and expect the subnest to still be open (clickable)
+    await browser.click('.level-1:nth-child(1)')
+    await browser.wait_for_text_to_equal('#output', 'nest.nested1')
+
+    await browser.click('.level-2:nth-child(2)')
+    await browser.wait_for_text_to_equal('#output', 'nest.subnest.sub2')
+
+    # Close subnest
+    await browser.click('div.level-1:nth-child(3) .tree-item-label')
+    await browser.wait_for_text_to_equal('#output', 'nest')
+
+    sub_containers = await browser.wait_for_elements_by_css_selector(
+        '.tree-sub-items'
+    )
+    assert len(sub_containers) == 1
