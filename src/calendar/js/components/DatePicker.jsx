a2c093d30644ca196ef3032a92040843044d0d52
t4rk <t4rk@outlook.com>
2020-07-21 21:11:52 -0400

    :hammer: Refactor DatePicker to functional component.

--- src/calendar/js/components/DatePicker.jsx
+++ src/calendar/js/components/DatePicker.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import Calendar from './Calendar';
 import {format} from 'date-fns';
@@ -12,49 +12,54 @@ import {toTimestamp} from 'commons';
  *     ``dazzler-calendar-date-picker``
  *     - ``picker``
  */
-export default class DatePicker extends React.Component {
-    componentWillMount() {
-        if (!this.props.value) {
-            this.props.updateAspects({
+const DatePicker = props => {
+    const {
+        class_name,
+        identity,
+        opened,
+        value,
+        date_format,
+        updateAspects,
+    } = props;
+
+    useEffect(() => {
+        if (!value) {
+            updateAspects({
                 value: toTimestamp(new Date()),
             });
         }
-    }
+    }, [value, updateAspects]);
 
-    render() {
-        const {class_name, identity, opened, value, date_format} = this.props;
-        if (value === undefined) return null;
-        return (
-            <div className={class_name} id={identity}>
-                <input
-                    type="text"
-                    onClick={e => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        this.props.updateAspects({opened: !opened});
-                    }}
-                    value={format(new Date(value * 1000), date_format)}
-                    readOnly={true}
-                />
-                {opened && (
-                    <div className="picker">
-                        <Calendar
-                            month_timestamp={value * 1000}
-                            _on_click={({day, month, year}) => {
-                                this.props.updateAspects({
-                                    value: toTimestamp(
-                                        new Date(year, month, day)
-                                    ),
-                                    opened: false,
-                                });
-                            }}
-                        />
-                    </div>
-                )}
-            </div>
-        );
-    }
-}
+    if (value === undefined) return null;
+
+    return (
+        <div className={class_name} id={identity}>
+            <input
+                type="text"
+                onClick={e => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    updateAspects({opened: !opened});
+                }}
+                value={format(new Date(value * 1000), date_format)}
+                readOnly={true}
+            />
+            {opened && (
+                <div className="picker">
+                    <Calendar
+                        month_timestamp={value * 1000}
+                        _on_click={({day, month, year}) => {
+                            updateAspects({
+                                value: toTimestamp(new Date(year, month, day)),
+                                opened: false,
+                            });
+                        }}
+                    />
+                </div>
+            )}
+        </div>
+    );
+};
 
 DatePicker.defaultProps = {
     date_format: 'DD MMM YYYY',
@@ -74,3 +79,5 @@ DatePicker.propTypes = {
      */
     updateAspects: PropTypes.func,
 };
+
+export default DatePicker;
