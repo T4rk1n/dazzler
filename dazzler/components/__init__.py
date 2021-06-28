b00d482ac3ebb750177ffbb16227eedf3237c465
t4rk <t4rk@outlook.com>
2021-06-28 01:09:35 -0400

    :bulb: Add missing components packages to docstring.

--- dazzler/components/__init__.py
+++ dazzler/components/__init__.py
@@ -1,8 +1,10 @@
 """
 Component packages of dazzler.
 
-- ``core``: Lightweight interactive components.
-- ``extra``: More components that may not be needed by all pages.
-- ``markdown``: Markdown and CodeBlock.
-- ``calendar``: Date & time related components.
+:auth: Login Form & Logout button
+:calendar: Date & time related components.
+:core: Lightweight interactive components.
+:extra: More components that may not be needed by all pages.
+:icons: Icons to use with the app, free packs with hundred of icons.
+:markdown: Markdown and CodeBlock.
 """
