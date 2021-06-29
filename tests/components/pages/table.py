9f20acee57ce10439c48accbafb12a4a25365984
t4rk <t4rk@outlook.com>
2021-06-29 00:58:32 -0400

    :rice: Add default style to core.Table

--- tests/components/pages/table.py
+++ tests/components/pages/table.py
@@ -9,11 +9,14 @@ page = Page(
     __name__,
     core.Container([
         core.Table(
-            [[1, 2, 3], [4, 5, 6]],
+            [[1, 2, 3], [4, 5, 6], [3, 4, 5]],
             headers=['one', 'two', 'three'],
-            caption='table',
+            caption='Table',
             include_row_number=True,
             row_number_start=7,
+            centered=True,
+            bordered=True,
+            size='large'
         ),
-    ])
+    ], class_name='row center')
 )
