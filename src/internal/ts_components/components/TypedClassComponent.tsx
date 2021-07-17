c6135c47f7f12bd1b4fa8c03aa24ed8b8eb6183d
t4rk <t4rk@outlook.com>
2021-07-16 21:47:58 -0400

    :rotating_light: Repeat ts test for class components.

---
+++ src/internal/ts_components/components/TypedClassComponent.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {TypedComponentProps} from '../../types';
+
+const defProps = {
+    default_required_str: 'default required',
+    default_str: 'default',
+    default_num: 3
+}
+
+/**
+ * Typed class component
+ */
+export default class TypedClassComponent extends React.Component<TypedComponentProps> {
+    render() {
+        const {children, class_name, style, identity, default_str, default_num} = this.props;
+        return (
+            <div id={identity} className={class_name} style={style}>
+                <div className='children'>{children}</div>
+                <div className='default_str'>{default_str}</div>
+                <div className='default_num'>{default_num}</div>
+            </div>
+        )
+    }
+
+    static defaultProps = defProps
+}
--- tests/components/pages/ts.py
+++ tests/components/pages/ts.py
@@ -4,20 +4,23 @@ from tests.components import ts_components as tsc
 
 page = Page(
     __name__,
-    tsc.TypedComponent(
-        'override',
-        children=core.Container('foobar'),
-        num=2,
-        text='foobar',
-        boo=True,
-        arr=[1, 2, 'mixed'],
-        arr_str=['foo', 'bar'],
-        arr_num=[7, 8, 9],
-        arr_obj_lit=[{'name': 'foo'}],
-        obj={'anything': 'possible'},
-        enumeration='foo',
-        union=7,
-        style={'border': '1px solid rgb(0,0,255)'},
-        class_name='other'
-    )
+    core.Container([
+        tsc.TypedComponent(
+            'override',
+            children=core.Container('foobar'),
+            num=2,
+            text='foobar',
+            boo=True,
+            arr=[1, 2, 'mixed'],
+            arr_str=['foo', 'bar'],
+            arr_num=[7, 8, 9],
+            arr_obj_lit=[{'name': 'foo'}],
+            obj={'anything': 'possible'},
+            enumeration='foo',
+            union=7,
+            style={'border': '1px solid rgb(0,0,255)'},
+            class_name='other'
+        ),
+        tsc.TypedClassComponent('class based', children='clazz')
+    ])
 )
