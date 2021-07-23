e5a8598aa934e69b597a67766b4902b023c35749
t4rk <t4rk@outlook.com>
2021-07-22 22:42:43 -0400

    :heavy_plus_sign: Add PyInstaller extra dep for electron.

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(
     extras_require={
-        'redis': ['aioredis==1.3.1']
+        'redis': ['aioredis==1.3.1'],
+        'electron': [
+            'PyInstaller==4.4'
+        ]
     }
 )
