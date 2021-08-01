136f545cc7a13e7d8805e251976ad24a656eb24b
T4rk1n <t4rk@outlook.com>
2021-07-31 23:14:33 -0400

    :white_check_mark: Add test_electron

---
+++ tests/electron/electron_app.py
@@ -0,0 +1,27 @@
+from dazzler import Dazzler
+from dazzler.system import Page
+from dazzler.electron import is_compiled
+from dazzler.components import core
+
+app = Dazzler(__name__)
+page = Page('main', core.Container([
+    core.Container('Main', identity='main'),
+    core.Button('Click', identity='clicker'),
+    core.Container(identity='output')
+]))
+app.add_page(page)
+
+
+# Should assert that bindings works
+@page.bind('clicks@clicker')
+async def on_click(ctx):
+    await ctx.set_aspect('output', children=f'Clicks {ctx.trigger.value}')
+
+
+if __name__ == '__main__':
+    if is_compiled():
+        app.start('')
+    else:
+        # TODO When reloading is present, the server is harder to kill
+        #  So need to assert that it can close and restart.
+        app.start('-v --reload')
---
+++ tests/electron/test_electron.py
@@ -0,0 +1,57 @@
+import os
+import shutil
+import tempfile
+import pytest
+import asyncio
+
+from selenium import webdriver
+
+from tests.tools import AsyncDriver
+
+
+@pytest.fixture()
+def electron_driver():
+    ns = {}
+
+    def initialize(binary_location):
+        options = webdriver.ChromeOptions()
+        options.binary_location = binary_location
+        driver = ns['driver'] = webdriver.Chrome(chrome_options=options)
+        return AsyncDriver(driver)
+
+    yield initialize
+
+    if 'driver' in ns:
+        # Need close not quit to cleanup the processes in electron.
+        ns['driver'].close()
+
+
+@pytest.mark.async_test
+async def test_electron_builder(electron_driver):
+    # Only way I found to automate the testing is by building
+    # the binary for selenium on chrome.
+    # Need to make sure the chrome-driver
+    # is the same version of chromium for the electron version.
+    output_dir = tempfile.mkdtemp('dazzler-electron')
+
+    try:
+        proc = await asyncio.create_subprocess_shell(
+            f'dazzler -c tests/electron/dazzler.toml '
+            f'electron-build tests/electron/electron_app.py'
+            f' -o {output_dir}'
+        )
+        await proc.communicate()
+        code = await proc.wait()
+
+        assert code == 0
+        binary_output = os.path.join(
+            output_dir, 'dist', 'linux-unpacked', 'dazzler-electron'
+        )
+        driver = electron_driver(binary_output)
+        await driver.wait_for_text_to_equal(
+            '#main', 'Main'
+        )
+        await driver.click('#clicker')
+        await driver.wait_for_text_to_equal('#output', 'Clicks 1')
+    finally:
+        shutil.rmtree(output_dir)
