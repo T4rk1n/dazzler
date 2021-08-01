53dd62b858bf97fef77300da9e810effe1a4a444
T4rk1n <t4rk@outlook.com>
2021-07-31 20:19:05 -0400

    :shirt: Fix pylint.

--- dazzler/tools/_ordered_set.py
+++ dazzler/tools/_ordered_set.py
@@ -11,12 +11,12 @@ class OrderedSet(collections.abc.MutableSet):
             self.add(i)
 
     # pylint: disable=arguments-differ
-    def add(self, x):
-        if x not in self._data:
-            self._data.append(x)
+    def add(self, value):
+        if value not in self._data:
+            self._data.append(value)
 
-    def discard(self, x):
-        self._data.remove(x)
+    def discard(self, value):
+        self._data.remove(value)
 
     def __contains__(self, x):
         return x in self._data
