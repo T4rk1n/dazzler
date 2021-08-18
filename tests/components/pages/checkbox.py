7cf12a3d397ae74e037aff78b9b455534044ceaf
T4rk1n <t4rk@outlook.com>
2021-08-17 21:35:36 -0400

    :white_check_mark: Add test_checkbox.

---
+++ tests/components/pages/checkbox.py
@@ -0,0 +1,27 @@
+from dazzler.system import Page, BindingContext, transforms as t
+from dazzler.components.core import Checkbox, Box, Button
+from dazzler.presets import PresetSize
+
+page = Page(
+    __name__,
+    Box([
+        Button('bind', identity='bind'),
+        Button('transform', identity='transform'),
+        Checkbox(checked=True, identity='checked'),
+        Checkbox(indeterminate=True, identity='indeterminate'),
+        Checkbox(checked=True, identity='dynamic'),
+        Checkbox(identity='preset-size', preset_size=PresetSize.larger),
+    ], column=True)
+)
+
+page.tie('clicks@transform', 'checked@dynamic').transform(
+    t.AspectValue('checked@dynamic').t(t.Not())
+)
+
+
+@page.bind('clicks@bind')
+async def on_bind_click(ctx: BindingContext):
+    checked = await ctx.get_aspect('dynamic', 'checked')
+    await ctx.set_aspect(
+        'dynamic', checked=not checked
+    )
