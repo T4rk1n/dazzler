50e83b85383a969098205dd8ef58c1ff51caf1c8
T4rk1n <t4rk@outlook.com>
2021-08-17 21:46:03 -0400

    :construction: Add common & presets styling api.

---
+++ src/commons/js/styling.ts
@@ -0,0 +1,67 @@
+import {AnyDict, CommonPresetsProps} from './types';
+import {join, mergeRight, pick} from 'ramda';
+import {snakeToCamelCase, transformKeys} from './casing';
+import {collectTruePropKeys} from './utils';
+
+const commonStyles = [
+    'color',
+    'background',
+    'padding',
+    'margin',
+    'overflow',
+    'height',
+    'max_height',
+    'width',
+    'max_width',
+    'font_family',
+    'font_size',
+    'font_style',
+    'text_align',
+    'border',
+    'border_radius',
+    'flex_grow',
+    'flex_shrink',
+    'align_self',
+    'display',
+];
+
+const booleanPresets = [
+    'rounded',
+    'bordered',
+    'centered',
+    'scrollable',
+    'hidden',
+    'unselectable',
+];
+
+const commonPresets = booleanPresets.concat([
+    'preset_color',
+    'preset_background',
+    'preset_size',
+]);
+const prefix = 'dazzler';
+
+export function getCommonStyles(obj: AnyDict, mergable?: AnyDict): AnyDict {
+    return mergeRight(
+        transformKeys(pick(commonStyles, obj), snakeToCamelCase),
+        mergable || {}
+    );
+}
+
+export function getPresetsClassNames(
+    obj: AnyDict,
+    ...classNames: string[]
+): string {
+    const presets: CommonPresetsProps = pick(commonPresets, obj);
+    const css = collectTruePropKeys(presets, booleanPresets);
+    if (presets.preset_color) {
+        css.push(`preset-color-${presets.preset_color}`);
+    }
+    if (presets.preset_size) {
+        css.push(`preset-size-${presets.preset_size}`);
+    }
+    if (presets.preset_background) {
+        css.push(`preset-background-${presets.preset_background}`);
+    }
+    return join(' ', css.map((p) => join('-', [prefix, p])).concat(classNames));
+}
--- src/commons/js/types.d.ts
+++ src/commons/js/types.d.ts
@@ -3,6 +3,44 @@ type StringDict = {[key: string]: string};
 type AnyDict = Dict<any>;
 type UpdateAspectFunc = (aspects: AnyDict) => void;
 
+type PresetSize =
+    | 'tiny'
+    | 'small'
+    | 'medium'
+    | 'large'
+    | 'larger'
+    | 'x-large'
+    | 'xx-large';
+
+type PresetColor =
+    | 'primary'
+    | 'primary-light'
+    | 'primary-dark'
+    | 'secondary'
+    | 'secondary-light'
+    | 'secondary-dark'
+    | 'tertiary'
+    | 'tertiary-light'
+    | 'tertiary-dark'
+    | 'danger'
+    | 'danger-light'
+    | 'danger-dark'
+    | 'warning'
+    | 'warning-light'
+    | 'warning-dark'
+    | 'success'
+    | 'success-light'
+    | 'success-dark'
+    | 'neutral'
+    | 'neutral-light'
+    | 'neutral-dark'
+    | 'dark'
+    | 'dark-light'
+    | 'dark-dark'
+    | 'darker'
+    | 'darker-light'
+    | 'darker-dark';
+
 export interface DazzlerProps {
     /**
      * Class name automatically added by dazzler api with a prefix for the
@@ -26,4 +64,136 @@ export interface DazzlerProps {
      * trigger ties & bindings.
      */
     updateAspects?: UpdateAspectFunc;
+}
+
+export interface CommonStyleProps {
+    /**
+     * Font color
+     */
+    color?: string;
+    /**
+     * Background color/image
+     */
+    background?: string;
+    /**
+     * Space around the content before the border
+     */
+    padding?: string | number;
+    /**
+     * Space after the element border
+     */
+    margin?: string | number;
+    /**
+     * Overflow the content and show a scrollbar.
+     */
+    overflow?: string;
+    /**
+     * Height of the component.
+     */
+    height?: string | number;
+    /**
+     * Maximum height before overflow.
+     */
+    max_height?: string | number;
+    /**
+     * Width of the component.
+     */
+    width?: string | number;
+    /**
+     * Maximum width before overflowing.
+     */
+    max_width?: string | number;
+    /**
+     * Font to use.
+     */
+    font_family?: string;
+    /**
+     * Size of the font.
+     */
+    font_size?: string | number;
+    /**
+     * Boldness
+     */
+    font_weight?: string | number;
+    /**
+     * Italic
+     */
+    font_style?: string;
+    /**
+     * Left center right.
+     */
+    text_align?: string;
+    /**
+     * Border around the component.
+     */
+    border?: string;
+    /**
+     * Round the corner of the component, needs a background or border.
+     */
+    border_radius?: string | number;
+    /**
+     * Cursor when the mouse is over the component.
+     */
+    cursor?: string;
+    /**
+     * Grow factor for flex item.
+     */
+    flex_grow?: number;
+    /**
+     * Shrink factor for flex item.
+     */
+    flex_shrink?: number;
+    /**
+     * Flex alignment for the component.
+     */
+    align_self?:
+        | 'auto'
+        | 'flex-start'
+        | 'flex-end'
+        | 'center'
+        | 'baseline'
+        | 'stretch';
+    /**
+     * CSS, How to display the component.
+     */
+    display?: string;
+}
+
+export interface CommonPresetsProps {
+    /**
+     * Round the edge of the container with borderRadius: 3px;
+     */
+    rounded?: boolean;
+    /**
+     * Apply default bordering with neutral dark grey,
+     */
+    bordered?: boolean;
+    /**
+     * Center the content in the middle of the container.
+     */
+    centered?: boolean;
+    /**
+     * Set the overflow content to scroll.
+     */
+    scrollable?: boolean;
+    /**
+     * Hide the component;
+     */
+    hidden?: boolean;
+    /**
+     * Do not allow for the text to be selected.
+     */
+    unselectable?: boolean;
+    /**
+     * Preset color to apply to the font.
+     */
+    preset_color?: PresetColor;
+    /**
+     * Preset color to apply to the background.
+     */
+    preset_background?: PresetColor;
+    /**
+     * Scale the size of the container using a preset.
+     */
+    preset_size?: PresetSize;
 }
