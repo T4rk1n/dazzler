8aa29c8291f7864c2b48649fb8b25ff9c47125c8
T4rk1n <t4rk@outlook.com>
2021-11-06 12:43:41 -0400

    :construction: Update pgapp.

--- tests/apps/samples/pgapp/app.py
+++ tests/apps/samples/pgapp/app.py
@@ -1,9 +1,6 @@
 from dazzler import Dazzler
-from dazzler.system import Page
-from dazzler.components.extra import PageMap
 
 app = Dazzler(__name__)
-app.add_page(Page('index', PageMap(), url='/'))
 
 if __name__ == '__main__':
     app.start('--debug -v --port=8199 --reload')
