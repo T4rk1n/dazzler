be1aec4143fb8752bb550b7201fcb79c15fed689
T4rk1n <t4rk@outlook.com>
2021-11-06 12:05:31 -0400

    :construction: Add click_indeterminate to checkbox.

--- src/core/js/components/Checkbox.tsx
+++ src/core/js/components/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useMemo, useRef} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {
     CommonPresetsProps,
     CommonStyleProps,
@@ -7,9 +7,22 @@ import {
 import {getCommonStyles, getPresetsClassNames} from 'commons';
 
 type CheckboxProps = {
+    /**
+     * Whether the checkbox is toggled or not.
+     */
     checked?: boolean;
+    /**
+     * Indeterminate display `-` meaning partial selection.
+     */
     indeterminate?: boolean;
+    /**
+     * Disable the checkbox
+     */
     disabled?: boolean;
+    /**
+     * Click on the checkbox will also set indeterminate status.
+     */
+    click_indeterminate?: boolean;
 } & DazzlerProps &
     CommonStyleProps &
     CommonPresetsProps;
@@ -22,8 +35,12 @@ const Checkbox = (props: CheckboxProps) => {
         style,
         updateAspects,
         class_name,
+        click_indeterminate,
         ...rest
     } = props;
+    const [clicks, setClicks] = useState(
+        0 + (checked ? 1 : 0) + (indeterminate ? 2 : 0)
+    );
     const css = useMemo(
         () => getPresetsClassNames(rest, class_name),
         [rest, class_name]
@@ -33,9 +50,33 @@ const Checkbox = (props: CheckboxProps) => {
 
     const onChange = useCallback(
         (event) => {
-            updateAspects({checked: event.target.checked});
+            if (click_indeterminate) {
+                const nClicks = clicks + 1;
+                setClicks(nClicks);
+                const payload: any = {};
+                const mod = nClicks % 3;
+                switch (mod) {
+                    case 0:
+                        payload.checked = false;
+                        payload.indeterminate = false;
+                        break;
+                    case 1:
+                        payload.checked = true;
+                        payload.indeterminate = false;
+                        break;
+                    case 2:
+                        payload.checked = false;
+                        payload.indeterminate = true;
+                        break;
+                    default:
+                        break;
+                }
+                updateAspects(payload);
+            } else {
+                updateAspects({checked: event.target.checked});
+            }
         },
-        [checked]
+        [checked, clicks]
     );
 
     useEffect(() => {
