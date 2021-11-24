a4664916baaa5a264e1f501816fbdddc7dc929cd
T4rk1n <t4rk@outlook.com>
2021-11-23 21:45:05 -0500

    :white_check_mark: Add test_charts

---
+++ tests/components/test_charts.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+@pytest.mark.async_test
+async def test_charts(start_page, browser):
+    from tests.components.pages.graphs import page
+
+    await start_page(page)
+
+    await browser.click('#prepend-data')
+    await browser.click('#append-data')
+    await browser.click('#insert-data')
+    await browser.click('#concat-data')
+    circles = await browser.wait_for_elements_by_css_selector(
+        '#line-chart circle'
+    )
+
+    assert len(circles) == 29
+    await browser.click('#delete-data')
+    circles = await browser.wait_for_elements_by_css_selector(
+        '#line-chart circle'
+    )
+    assert len(circles) == 28
